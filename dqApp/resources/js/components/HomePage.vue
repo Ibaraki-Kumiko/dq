c80d75306c3282c824378e47a1759dc27a3f6ac4
Svetlana <trivium.ibaraki@gmail.com>
2023-07-26 16:49:55 +0200

    modify HomePage

--- dqApp/resources/js/components/HomePage.vue
+++ dqApp/resources/js/components/HomePage.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>Hi</h1>
+    <section class="page-wrapper">
+        <div class="content-wrapper">
+            <h2 class="error-subtitle text-center">Something went wrong...</h2>
+            <p class="error-description text-center">We are experiencing an unexpected server error. We are working on fixing the problem. Be back soon.</p>
+        </div>
+    </section>
 </template>
 
 <script>

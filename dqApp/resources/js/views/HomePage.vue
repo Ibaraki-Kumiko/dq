3758e294b821d61c01451d8d0927715f5635c575
svetlana.a <berger8sv@gmail.com>
2023-10-25 15:39:40 +0200

    add style for the HomePage

--- dqApp/resources/js/views/HomePage.vue
+++ dqApp/resources/js/views/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="page-wrapper">
         <div class="content-wrapper">
-            <h1 class="title font-white"><b>Comming Soon</b></h1>
-            <p class="desc font-white">Our website is currently undergoing scheduled maintenance.
+        <div class="centered-text">
+            <h1 class="title"><b>Coming Soon</b></h1>
+            <p class="desc">Our website is currently undergoing scheduled maintenance.
                 We Should be back shortly. Thank you for your patience.</p>
+        </div>
         </div>
     </section>
 </template>
@@ -15,5 +17,17 @@ export default {
 </script>
 
 <style scoped>
+.centered-text {
+    padding-top: 20%;
+    height: 60vh;
+    text-align: center;
+}
 
+.title {
+    font-size: 3.25rem;
+}
+
+.desc {
+    font-size: 2rem;
+}
 </style>
